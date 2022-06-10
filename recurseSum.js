2f6005d66df28594b9d68905e430c6d26320bbc1
Lewis Powers <lpowers1985@gmail.com>
2022-06-10 15:34:03 -0700

    solution to interview prep

---
+++ recurseSum.js
@@ -0,0 +1,16 @@
+function superDigit(n, k) {
+  let sumKTimes = (sumOfN(n) * k).toString();
+  return sumOfN(sumKTimes)
+  function sumDigitsOfStr(numStr) {
+    return numStr.split('').reduce((acc, curr) => {
+      return acc + Number(curr);
+    }, 0).toString();
+  }
+  function sumOfN(nStr) {
+    while (nStr.length > 1) nStr = sumDigitsOfStr(nStr);
+    return nStr;
+  }
+}
+console.log(superDigit('148', 3));
+console.log(superDigit('4757362', 111));
+console.log(superDigit('4757362', 10000));
