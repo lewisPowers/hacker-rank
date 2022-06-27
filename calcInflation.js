6f163590163d151d85c115d808809c61d91962a7
Lewis Powers <lpowers1985@gmail.com>
2022-06-27 15:50:20 -0700

    commented log statements

--- calcInflation.js
+++ calcInflation.js
@@ -8,9 +8,9 @@ function calculateInflation(baseCost, years, percentPerYear) {
   return `Total from $${baseCost} at ${percentPerYear}% per year over ${totalYears} years: $${Number(total.toFixed())}`
 }
 
-console.log(calculateInflation(61000, 60, 2.85))
-console.log(calculateInflation(61000, 60, 5.7))
-console.log(calculateInflation(10, 5, 15))
+// console.log(calculateInflation(61000, 60, 2.85))
+// console.log(calculateInflation(61000, 60, 5.7))
+// console.log(calculateInflation(10, 5, 15))
 
 function averageInflationPerYear(baseCost, finalCost, yearsApart) {
   let difference = finalCost - baseCost;
@@ -22,7 +22,7 @@ function averageInflationPerYear(baseCost, finalCost, yearsApart) {
   average = average || (median + (baseAverage - median) / 2).toFixed(3) * 1
   return `From $${baseCost} to $${finalCost} over ${yearsApart} years: ${average * 100}% average yearly inflation`
 }
-console.log(averageInflationPerYear(10,20,5))
-console.log(averageInflationPerYear(100,108.6,1))
-console.log(averageInflationPerYear(61000,425000,60))
-console.log(averageInflationPerYear(1000,100000,100))
+// console.log(averageInflationPerYear(10,20,5))
+// console.log(averageInflationPerYear(100,108.6,1))
+// console.log(averageInflationPerYear(61000,425000,60))
+// console.log(averageInflationPerYear(1000,100000,100))
